5fadf524073450988fab3dba9aa00fe4207e953e
luis-122003 <mmostanman@gmail.com>
2026-05-31 19:11:39 -0500

    corrreccion de vercionamiento

--- mobile/windows/runner/main.cpp
+++ mobile/windows/runner/main.cpp
@@ -1,5 +1,5 @@
-#include <flutter/dart_project.h>
-#include <flutter/flutter_view_controller.h>
+#include "../flutter/dart_project.h"
+#include "../flutter/flutter_view_controller.h"
 #include <windows.h>
 
 #include "flutter_window.h"
